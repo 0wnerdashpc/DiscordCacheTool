35c0428b48d1f1d932614057338994b703685fc2
Jake <0wnerdashpc@gmail.com>
2019-03-01 11:20:28 -0500

    Update discordcachetool.py
    Script is usable, now public for others to use.

--- discordcachetool.py
+++ discordcachetool.py
@@ -1,13 +1,18 @@
-import os, shutil, glob
+import os, shutil
 
 try:
     user = input('Please enter your PC username, you can find this at C:\\Users\\NAMEHERE\n')
     source_dir = 'C:/Users/'+user+'/AppData/Roaming/Discord/Cache/'
     target_dir = 'C:/Users/'+user+'/Desktop/DiscordImgs/'
-    shutil.copytree(source_dir, target_dir)
-    for filename in glob.iglob(os.path.join(target_dir, '*.*')):
-        os.rename(filename, filename+'.png')
+    try:
+        shutil.copytree(source_dir, target_dir)
+    except:
+        print('DiscordImgs folder found, removing and recreating the directory.')
+        shutil.rmtree(target_dir)
+        shutil.copytree(source_dir, target_dir)
+        print('Done, continuing.')
+    for filename in os.listdir(target_dir):
+        os.rename(os.path.join(target_dir, filename), os.path.join(target_dir, filename+'.jpg'))
     print('Complete, you can now find those files on your desktop.')
-except:
-    print('Error Encountered\nDelete the discordimg folder\nClose out of Discord\nRun the script again.')
-    pass
+except: #While this most likely a bad practice, the only errors encountered during testing were No.13 errors, which are access errors.
+    print('Error Encountered\nClose out of Discord\nRun the script again.')
